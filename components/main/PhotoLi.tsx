4eac9d830a62c70920cdf711f98244008c3082dc
ksh_dd <zocbdjf@gmail.com>
2021-05-04 17:19:05 +0900

    5/4 리스트에 가이드 이름 추가 / 리스트에 BEST 심볼추가

--- components/main/PhotoLi.tsx
+++ components/main/PhotoLi.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 export type TPhotoLi = {
     id: string;
@@ -6,23 +6,34 @@ export type TPhotoLi = {
     title: string;
     subTitle: string;
     [key: string]: any;
-}
+};
 
 interface IProp extends TPhotoLi {
     onClickImg: () => void;
 }
 
-export const PhotoLi: React.FC<IProp> = ({ category, title, subTitle, onClickImg }) => {
-    return <li className="list_in">
-        <div className="img" onClick={onClickImg} style={{
-            backgroundImage: `url(/img/test_img_01.jpg)`
-        }}></div>
-        <div className="box">
-            <div className="category"><span>{category}</span></div>
-            <div className="title">{title}</div>
-            <div className="subTitle">
-                {subTitle}
+export const PhotoLi: React.FC<IProp> = ({
+    category,
+    title,
+    subTitle,
+    onClickImg,
+}) => {
+    return (
+        <li className="list_in">
+            <div
+                className="img"
+                onClick={onClickImg}
+                style={{
+                    backgroundImage: `url(/img/test_img_01.jpg)`,
+                }}
+            ></div>
+            <div className="box">
+                <div className="category">
+                    <span>{category}</span>
+                </div>
+                <div className="title">{title}</div>
+                <div className="subTitle">{subTitle}</div>
             </div>
-        </div>
-    </li >;
+        </li>
+    );
 };
